7be3424d931945770ac357acf7e2144b16e11071
Anand Nevase <nevase.anand@gmail.com>
2020-10-30 23:27:47 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  # This module is now only being tested with Terraform 0.13.x. However, to make upgrading easier, we are setting
+  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
+  # forwards compatible with 0.13.x code.
+  required_version = ">= 0.12.26"
+}
+
+variable "output" {
+  type = string
+  default = "Hello, World"
+}
+
+variable "msg" {
+  type = string
+  default = "!!"
+}
+
+output "hello_world" {
+  value =  "${var.output} ${var.msg}"
+}
